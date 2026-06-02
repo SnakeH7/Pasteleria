e83f20dc816989738a03fbf738a9fadf153e6965
SnakeH7 <angeljosuemr08@gmail.com>
2026-06-01 18:43:19 -0600

    Agregar archivos de proyecto.

---
+++ Data/PasteleriaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Pasteleria.Models;
+
+namespace Pasteleria.Data
+{
+    public class PasteleriaContext: DbContext
+    {
+        //El constructor recibe la configuración de la conexion
+        public PasteleriaContext(DbContextOptions<PasteleriaContext> options) : base(options)
+        {
+
+        }
+    }
+}
