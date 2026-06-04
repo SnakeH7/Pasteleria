4520df7fc8fddce7b9f7e69a68af7700284048ee
SnakeH7 <angeljosuemr08@gmail.com>
2026-06-04 15:37:37 -0600

    Implementacion de inicio/registro de sesion mediante UsuarioDTO más seguridad con BCrypt

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pasteleria.Data;
+using Pasteleria.Models;
+using BCrypt.Net;
+
+namespace Pasteleria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly PasteleriaContext _context;
+
+        public UsuariosController(PasteleriaContext context) //Colocamos el contexto y la conexión en nuestro controller
+        {
+            _context = context;
+        }
+
+        [HttpPost("registrar")] //ENDPOINT DEL REGISTRO
+        public async Task<ActionResult> Registrar(UsuarioDTO peticion)
+        {
+            //Validacion sencilla para evitar duplicar correos y por seguridad
+            bool usuarioExiste = await _context.Usuarios.AnyAsync(u => u.Correo == peticion.Correo); //Busca en la base un correo igual
+
+            if (usuarioExiste)
+            {
+                return BadRequest("El Correo ya está en uso");
+            }
+
+            string hashGenerado = BCrypt.Net.BCrypt.HashPassword(peticion.Contrasena); //BCrypt Hashea la contraseña para mayor seguridad
+
+            var nuevoUsuario = new Usuarios //Objeto para la DB
+            {
+                Correo = peticion.Correo,
+                ContrasenaHash = hashGenerado
+            }; //Dentro se guarda la contraseña hasheada solamente
+
+            _context.Usuarios.Add(nuevoUsuario); 
+            await _context.SaveChangesAsync(); //Await espera a que el método termine
+            return Ok("¡Usuario registrado con éxito!");
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Iniciar(UsuarioDTO peticion)
+        {
+            var usuarioEnBD = await _context.Usuarios.FirstOrDefaultAsync(u => u.Correo == peticion.Correo);
+
+            if(usuarioEnBD == null)
+            {
+                return BadRequest("Correo o contraseña incorrectos");
+            }
+
+            bool contrasenaCorrecta = BCrypt.Net.BCrypt.Verify(peticion.Contrasena, usuarioEnBD.ContrasenaHash);
+
+            if(!contrasenaCorrecta)
+            {
+                return BadRequest("¡EL correo o la contraseña es incorrectos!");
+            }
+
+            return Ok("¡Bienvenido!");
+        }
+    }
+}
---
+++ Models/UsuarioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Pasteleria.Models
+{
+    public class UsuarioDTO //Los DTO's funcionan como clases mensajeras para llevar los datoss crudos que arrojen 
+    {
+        public string Correo { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
