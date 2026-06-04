933e35b45610c0eed67f574de04dcb81219611bd
SnakeH7 <angeljosuemr08@gmail.com>
2026-06-04 01:06:15 -0600

    Controllers Categorias y Postres agregados

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using Pasteleria.Data;
+using Pasteleria.Models;
 namespace Pasteleria.Controllers
 {
-    public class CategoriasController : Controller
+    [Route("api/[controller]")] //La ruta de mi sitio 
+    [ApiController]
+    public class CategoriasController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly PasteleriaContext _context; //Readonly se asegura de que una vez que haya recibido un valor _context
+        //ya no puede cambiar la variable, ni siquiera de manera accidental 
+
+        public CategoriasController(PasteleriaContext context) //Inyección de dependencias/clean architecture
         {
-            return View();
+            _context = context;
         }
+        //Endpoint
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categorias>>> GetCategorias() //Asyn Task quiere decir que la tarea es asíncrona permitiendo liberar el hilo para más tareas
+        {
+            //IEnumerable es una colección en forma de lista de tipo "Categorias"
+            var categorias = await _context.Categorias.ToListAsync(); //_context categorías es la tabla de la base de datos y ToListAsync se encarga de crear una Query SQL
+            return Ok(categorias); //OK devuelve 200. Await pausa el método hasta que regrese la lista transformada sin bloquear el server
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categorias>> PostCategorias(Categorias nuevaCategoria)
+        {
+            _context.Categorias.Add(nuevaCategoria); //utilizamos _context para viajar hasta la db
+            await _context.SaveChangesAsync(); //await espera a que se realicen los cambios (SaveChangesAsync()) para enviarlos a SQL Server
+            return Ok(nuevaCategoria); //Se devuelve la categoría recien agregada 
+        }
+        
     }
 }
---
+++ Controllers/PostresController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pasteleria.Data;
+using Pasteleria.Models;
+namespace Pasteleria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostresController : ControllerBase
+    {
+        private readonly PasteleriaContext _context;
+
+        public PostresController(PasteleriaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Postres>>> GetPostres()
+        {
+            var postres = await _context.Postres.Include( p => p.Categorias).ToListAsync();
+            return Ok(postres);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Postres>> PostPostre(Postres nuevoPostre)
+        {
+            // Verificamos si la categoría que nos enviaron realmente existe en SQL Server
+            var categoriaExiste = await _context.Categorias.FindAsync(nuevoPostre.IdCategoria);
+
+            if (categoriaExiste == null)
+            {
+                return BadRequest("Error: La categoría ingresada no existe.");
+            }
+
+            _context.Postres.Add(nuevoPostre);
+            await _context.SaveChangesAsync();
+
+            return Ok(nuevoPostre);
+        }
+    }
+}
